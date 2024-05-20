8b4e12ac96d89eb61c58631d9d853199c9adf705
wtiinfo <wanbrwan@gmail.com>
2024-05-20 14:14:31 -0300

    Primeira pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
+import StartScreen from './components/StartScreen';
 
 function App() {
 
   return (
    <div>
-      <h2>Secret Word</h2>
+      <StartScreen />
    </div>
   )
 }
