cb0498ff611b13903edb979e3726f8f887767683
wtiinfo <wanbrwan@gmail.com>
2024-05-20 14:00:10 -0300

    Pagina inicial, css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+   <div>
       <h2>Secret Word</h2>
-  
-    </>
+   </div>
   )
 }
 
-export default App
+export default App;
